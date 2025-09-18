a31f66200adfe233cd3617ccdc39535170ffe090
lambrski <lambrski@013.net.il>
2025-09-18 13:34:44 +0300

    Remove טיוטה directory

--- טיוטה/גירסה 4 - מחולק/app/database.py
+++
@@ -1,8 +0,0 @@
-# app/database.py
-from sqlmodel import create_engine, SQLModel
-
-DB_FILE = "db.sqlite"
-engine = create_engine(f"sqlite:///{DB_FILE}", echo=False)
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
